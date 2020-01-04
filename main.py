1b29370a71780bd0540dd67e3d8c721ff09fb883
vasiliy566 <vasyaisaev31@gmail.com>
2020-01-04 23:24:36 +0300

    initial

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Simple Bot to reply to Telegram messages.
+This is built on the API wrapper, see echobot2.py to see the same example built
+on the telegram.ext bot framework.
+This program is dedicated to the public domain under the CC0 license.
+"""
+from secure import TOKEN
+
+import telebot
+
+bot = telebot.TeleBot('TOKEN')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+bot.polling()
